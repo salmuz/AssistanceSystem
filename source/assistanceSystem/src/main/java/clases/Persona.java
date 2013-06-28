1e5ce2e432dfc0c4cb3a0fe0e4cf576c42897489
marestep <step.jc.18@github.com>
2013-06-28 15:56:47 -0500

    interfaz

--- source/assistanceSystem/src/main/java/clases/Persona.java
+++ source/assistanceSystem/src/main/java/clases/Persona.java
@@ -15,30 +15,18 @@ public class Persona implements Serializable {
         this.nombre = nom;
     }
 
-    /**
-     * @return the dni
-     */
     public String getDni() {
         return dni;
     }
 
-    /**
-     * @param dni the dni to set
-     */
     public void setDni(String dni) {
         this.dni = dni;
     }
 
-    /**
-     * @return the nombre
-     */
     public String getNombre() {
         return nombre;
     }
 
-    /**
-     * @param nombre the nombre to set
-     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
